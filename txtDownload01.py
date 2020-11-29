e9a9a19901a87ae1f6fce6b1f34fb822a77d7cb6
CPC <www.chenpengchong@gmail.com>
2020-11-29 18:55:55 +0800

    优化

--- txtDownload01.py
+++ txtDownload01.py
@@ -6,29 +6,38 @@ url = 'http://www.iddwx.com/soft1/'
 
 response = requests.get(url)
 content = response.content.decode('UTF-8')
-print(content)
+# print(content)
 html = etree.HTML(content)
 
-text_name_list = html.xpath('/html/body/div[@class="globalBox"]/div[@id="pageMain"]/div[@class="mainBody"]/div[@class="pageMainArea"]/div[@id="listbox"]/ul[@id="mainlistUL"]/div[@class="mainListInfo"]/div/span/a')
-text_size_list = html.xpath('/html/body/div[@class="globalBox"]/div[@id="pageMain"]/div[@class="mainBody"]/div[@class="pageMainArea"]/div[@id="listbox"]/ul[@id="mainlistUL"]/div[@class="mainListBottom"]/div[@class="mainAccredit"]/text()')
+txt_name_list = html.xpath('/html/body/div[@class="globalBox"]/div[@id="pageMain"]/div[@class="mainBody"]/div[@class="pageMainArea"]/div[@id="listbox"]/ul[@id="mainlistUL"]/div[@class="mainListInfo"]/div/span/a')
+txt_size_list = html.xpath('/html/body/div[@class="globalBox"]/div[@id="pageMain"]/div[@class="mainBody"]/div[@class="pageMainArea"]/div[@id="listbox"]/ul[@id="mainlistUL"]/div[@class="mainListBottom"]/div[@class="mainAccredit"]/text()')
+print(type(txt_name_list))
+
+for a in txt_name_list:
+    print(a.attrib.get(a))
 
 
-
-# for a in text_name_list:
+# for a in txt_name_list:
 #     print(a.attrib)
 
-text_info = [href_title.attrib for href_title in text_name_list]
+#文本详情
+txt_info = [href_title.attrib for href_title in txt_name_list]
 
+#将文件大小加入
 i=0
-for info in text_info:
-    info['size'] = text_size_list[i]
+for info in txt_info:
+    info['size'] = txt_size_list[i]
     i+=1
 
+# for info in txt_info:
+#     print(type(info))
 
 
-print(text_info)
 
-# for b in text_size_list:
+
+# print(txt_info)
+
+# for b in txt_size_list:
 #     print(b)
 
 
