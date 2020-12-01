fcf0e906cfdab54e0be4147e68f351bb2526ea91
CPC <www.chenpengchong@gmail.com>
2020-12-01 11:20:41 +0800

    优化

--- txtDownload01.py
+++ txtDownload01.py
@@ -3,6 +3,7 @@ import jsonpath
 from lxml import etree
 
 url = 'http://www.iddwx.com/soft1/'
+url = 'http://www.iddwx.com/soft2/'
 
 response = requests.get(url)
 content = response.content.decode('UTF-8')
@@ -11,14 +12,9 @@ html = etree.HTML(content)
 
 txt_name_list = html.xpath('/html/body/div[@class="globalBox"]/div[@id="pageMain"]/div[@class="mainBody"]/div[@class="pageMainArea"]/div[@id="listbox"]/ul[@id="mainlistUL"]/div[@class="mainListInfo"]/div/span/a')
 txt_size_list = html.xpath('/html/body/div[@class="globalBox"]/div[@id="pageMain"]/div[@class="mainBody"]/div[@class="pageMainArea"]/div[@id="listbox"]/ul[@id="mainlistUL"]/div[@class="mainListBottom"]/div[@class="mainAccredit"]/text()')
+txt_introduction_list = html.xpath('/html/body/div[@class="globalBox"]/div[@id="pageMain"]/div[@class="mainBody"]/div[@class="pageMainArea"]/div[@id="listbox"]/ul[@id="mainlistUL"]/div[@id="soft_content_1"]/text()')
 # print(type(txt_name_list))
 
-# for a in txt_name_list:
-#     print(a.attrib)
-
-
-# for a in txt_name_list:
-#     print(a.attrib)
 
 #文本详情
 txt_info_list = [href_title.attrib for href_title in txt_name_list]
@@ -27,22 +23,27 @@ txt_info_list = [href_title.attrib for href_title in txt_name_list]
 i=0
 for info in txt_info_list:
     info['size'] = txt_size_list[i]
+    info['introduction'] = txt_introduction_list[i]
     i+=1
-print(txt_info_list)
+# print(txt_info_list)
 
 #去除小于3MB
-for info in txt_info_list:
-    if info.get('size').endswith('KB'):
-        print(info)
-        txt_info_list.remove(info)
-print(txt_info_list)
-print('-----------------------------')
-for info in txt_info_list:
-    print(info)
-    if int(info.get('size').split('.')[0]) < 3:
-        print(info)
-        txt_info_list.remove(info)
-        continue
+
+#1.带有KB的删掉
+with_KBs = [info for info in txt_info_list if info.get('size').endswith('KB')]
+# for data in with_KBs:
+#     print(data)
+
+for with_KB_data in with_KBs:
+    txt_info_list.remove(with_KB_data)
+
+#2.小于3MB的删掉
+lt_3MBs = [info for info in txt_info_list if int(info.get('size').split('.')[0]) < 3]
+
+for lt_3MB_data in lt_3MBs:
+    txt_info_list.remove(lt_3MB_data)
+
+
 
 
 
@@ -50,7 +51,9 @@ for info in txt_info_list:
 # print(len(txt_info_list))
 
 
-print(txt_info_list)
+print('---------------------------')
+for data in  txt_info_list:
+    print(data)
 
 # for b in txt_size_list:
 #     print(b)
